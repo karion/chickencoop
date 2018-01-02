823debf1e679d9cf534dfa6c07996867c8ad97de
Michał Kurzeja <michal@idesigni.co.uk>
2018-01-02 22:16:39 +0100

    setup correct command

--- bin/console.php
+++ bin/console.php
@@ -1,12 +1,12 @@
 #!/usr/bin/env php
 <?php
+
 require __DIR__.'/../vendor/autoload.php';
 
-//use Acme\Command\DefaultCommand;
 use Karion\Chickencoop\Command\MonteCarlo;
 use Symfony\Component\Console\Application;
 
-$application = new Application('echo', '1.0.0');
+$application = new Application('MonteCarlo', '1.0.0');
 $command = new MonteCarlo();
 
 $application->add($command);
