ed3b036387e56b984779d358e2ae6c3550e2c99e
Michał Kurzeja <michal@idesigni.co.uk>
2018-01-07 23:38:43 +0100

    add first strategy

--- src/Strategy/NoSwitchStrategy.php
+++ src/Strategy/NoSwitchStrategy.php
@@ -2,20 +2,25 @@
 
 namespace Karion\Chickencoop\Strategy;
 
-
 use Karion\Chickencoop\Chickencoop;
 
-interface StrategyInterface
+class NoSwitchStrategy implements StrategyInterface
 {
     /**
      * @param Chickencoop $chickencoop
      * @return bool is switch was done on chickencoop
      */
-    public function makeSwitch(Chickencoop $chickencoop) :bool;
+    public function makeSwitch(Chickencoop $chickencoop): bool
+    {
+        return false;
+    }
 
     /**
      * get strategy name
      * @return string
      */
-    public function getName() : string;
+    public function getName(): string
+    {
+        return "no_switch";
+    }
 }
