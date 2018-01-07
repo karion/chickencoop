3680d3037e77aca3e4bc0a145dc40a5ab2cd20c5
Michał Kurzeja <michal@idesigni.co.uk>
2018-01-08 00:14:28 +0100

    first working version

--- src/Command/MonteCarlo.php
+++ src/Command/MonteCarlo.php
@@ -2,8 +2,16 @@
 
 namespace Karion\Chickencoop\Command;
 
+use Karion\Chickencoop\Chickencoop;
+use Karion\Chickencoop\Game;
+use Karion\Chickencoop\Service\DiceService;
+use Karion\Chickencoop\Service\ThrowService;
+use Karion\Chickencoop\Strategy\NoSwitchStrategy;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class MonteCarlo extends Command
@@ -12,13 +20,41 @@ class MonteCarlo extends Command
     {
         $this
             ->setName('karion:chickencoop')
+            ->setDefinition(
+                new InputDefinition(array(
+                    new InputArgument(
+                        'games',
+                        InputArgument::OPTIONAL,
+                        "",
+                        1000
+                    )
+                ))
+            )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        dump("start");
-        // ...
+        $gamesLimit = (int) $input->getArgument('games');
+        $gamesCount = 0;
+
+        $gameEngine = new Game(
+            new ThrowService(new DiceService()),
+            [
+                new NoSwitchStrategy()
+            ]
+        );
+
+        while ($gamesCount <= $gamesLimit) {
+            $chickencoop = new Chickencoop();
+
+            $stats = $gameEngine->play($chickencoop, 'no_switch');
+
+            $output->writeln($stats);
+
+            $gamesCount++;
+            unset($chickencoop);
+        }
     }
 
 }
